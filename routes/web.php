313ea128aabfdfee0a36a909bd91d2a4be2f7f71
hunter666hz <hzhunter7@gmail.com>
2024-02-23 10:47:45 +0100

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/hello/{id}',function(int $id){
+    // ddd($id);
+    return response("<h1>hello there ($id) </h1>",200);
+})->where('id','[0-9]+');
+
+Route::get('search/',function(Request $req){
+    
 });

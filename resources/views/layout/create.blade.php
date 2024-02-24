ea2f38ab19a9766637438ad6cc19cbe49e26dbaa
hunter666hz <hzhunter7@gmail.com>
2024-02-24 23:12:12 +0100

    new file:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	new file:   public/css/app.css 	modified:   resources/css/app.css 	modified:   resources/views/app.blade.php 	modified:   resources/views/layout/create.blade.php 	new file:   tailwind.config.js

--- resources/views/layout/create.blade.php
+++ resources/views/layout/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <form action="/posts" method="POST">
         @csrf
-        <h4 class="text-red-500">hello there </h4>
+        <h4 class=" text-orange-400">hello there </h4><input type="text">
         <textarea name="content" id="" cols="30" rows="10" placeholder="content">  </textarea>
         <button type="submit">share</button>
 
